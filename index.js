63996b5017637d23c4c83e63f0362842863f7c5e
Lionel Bertrand <lionel.bertrand.prestataire@sfr.com>
2016-10-13 14:37:40 +0200

    try launch apt-get with socket ask password sudo

--- index.js
+++ index.js
@@ -42,35 +42,16 @@ class PluginSample extends Plugin {
         //   console.log(Object.keys(this.dependencies.server));
         // })
         if(this.dependencies.server.socket){
-          this.dependencies.server.socket.on('askSudo:response', (data) => {
-            // echo <password> | sudo -S <command>
-            //
+          this.dependencies.server.socket.on('askSudo:response', (data, fc) => {
             const exec = require('child_process').exec;
             exec('echo ' + data.password + ' | sudo -S apt-get install motion', (error, stdout, stderr) => {
               if (error) {
-                console.error(`exec error: ${error}`);
                 return;
               }
-              console.log(`stdout: ${stdout}`);
-              console.log(`stderr: ${stderr}`);
+              fc({success: true});
             });
 
-            // let ls = spawn('echo', [data.password, '|', 'sudo', '-S', 'apt-get', 'install', 'motion']);
-            //
-            // ls.stdout.on('data', (data) => {
-            //   console.log(`stdout: ${data}`);
-            // });
-            //
-            // ls.stderr.on('data', (data) => {
-            //   console.log(`stderr: ${data}`);
-            // });
-            //
-            // ls.on('close', (code) => {
-            //   console.log(`child process exited with code ${code}`);
-            // });
-
-            this.dependencies.server.socket.off('askSudo:response');
-            // console.log(data);
+            // this.dependencies.server.socket.off('askSudo:response');
           });
 
           this.dependencies.server.socket.emit('askSudo');
